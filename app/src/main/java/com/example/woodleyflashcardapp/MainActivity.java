4889bfd632728eaac03564e4904592240fb97e63
KingLaine <gelinw3@gmail.com>
2021-02-20 10:51:03 -0800

    Initial commit

---
+++ app/src/main/java/com/example/woodleyflashcardapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.woodleyflashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView questionTextView = findViewById(R.id.flashcard_question);
+        TextView answerTextView = findViewById(R.id.flashcard_answer);
+
+        questionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+    }
+}
