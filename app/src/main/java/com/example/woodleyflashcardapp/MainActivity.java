dcb117721bdacb9d06aba3cbf86e3c70928a1f58
KingLaine <gelinw3@gmail.com>
2021-02-21 00:39:04 -0800

    Add three buttons and highlight red if false green if tue

--- app/src/main/java/com/example/woodleyflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/woodleyflashcardapp/MainActivity.java
@@ -11,8 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         TextView questionTextView = findViewById(R.id.flashcard_question);
         TextView answerTextView = findViewById(R.id.flashcard_answer);
+        TextView answers1 = findViewById(R.id.answer1);
+        TextView answers2 = findViewById(R.id.answer2);
+        TextView answers3 = findViewById(R.id.answer3);
 
         questionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +26,38 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        answerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                questionTextView.setVisibility(View.VISIBLE);
+                answerTextView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // Change answers background color to verify if true or false
+
+        answers1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              findViewById(R.id.answer1).setBackgroundColor(getResources().getColor(R.color.my_red_color));
+
+            }
+        });
+        answers2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.my_red_color));
+
+            }
+        });
+        answers3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer3).setBackgroundColor(getResources().getColor(R.color.my_green_color));
+
+            }
+        });
+
 
     }
 }
