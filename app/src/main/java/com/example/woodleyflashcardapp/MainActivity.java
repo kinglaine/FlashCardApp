fbc7336deff7adfa105d7e61d3e26494ca113e3f
KingLaine <gelinw3@gmail.com>
2021-02-26 22:08:33 -0800

    Add hide and show button to hide or show answers

--- app/src/main/java/com/example/woodleyflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/woodleyflashcardapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.woodleyflashcardapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +13,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         TextView questionTextView = findViewById(R.id.flashcard_question);
         TextView answerTextView = findViewById(R.id.flashcard_answer);
         TextView answers1 = findViewById(R.id.answer1);
         TextView answers2 = findViewById(R.id.answer2);
         TextView answers3 = findViewById(R.id.answer3);
+        ImageView eyebutton = findViewById(R.id.toggle_choices_visibility);
+
+
+        eyebutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                boolean isShowingAnswers = true;
+                eyebutton.setImageResource(R.drawable.eyeoff);
+                if (isShowingAnswers) {
+                    answers1.setVisibility(View.VISIBLE);
+                    answers2.setVisibility(View.VISIBLE);
+                    answers3.setVisibility(View.VISIBLE);
+                    eyebutton.setImageResource(R.drawable.eyeoff);
+                    isShowingAnswers = false;
+
+                } if (!isShowingAnswers){
+                    answers1.setVisibility(View.INVISIBLE);
+                    answers2.setVisibility(View.INVISIBLE);
+                    answers3.setVisibility(View.INVISIBLE);
+                    eyebutton.setImageResource(R.drawable.eyeon);
+
+                }
+
+
+            }
+        });
+
+
+
+
 
         questionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
